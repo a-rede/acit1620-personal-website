5bbacc75b54bca5b5e3fb0c89761c16db4e29bd6
a-rede <abell.rede1@outlook.com>
2022-11-08 11:56:37 -0800

    Define a constant array storing three course objects

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Define a array containing course information
+let course1 = {
+    code: "ACIT 1620",
+    name: "Web Fundamental Technologies "
+}
+
+let course2 = {
+    code: 'ACIT 1515',
+    name: 'Scripting for IT'
+}
+
+let course3 = {
+    code: 'ACIT 1420',
+    name: 'Introduction to Systems Administration'
+}
+
+const courseList = [course1]

bc9a9bc5073d15cf2332f013ad383ca8c08e2dcf
a-rede <abell.rede1@outlook.com>
2022-11-10 12:03:10 -0800

    Complete script to retrieve input and store in the array if input is not present

--- js/script.js
+++ js/script.js
@@ -18,6 +18,22 @@ const courseList = [course1, course2, course3]
 
 //Prompting user for input, checking to see if input is invalid
 //Input is invalid if input is a string or less than a 4 digit value
+let input = '';
 do {
     input = prompt('Enter a 4-digit number: ')
 } while (input.length != 4 || isNaN(Number(input)))
+
+//Writing a loop to iterate through courseList and match code number with user input
+let flagState = false;
+for (let course of courseList) {
+    if (course.code.includes(input)) {
+        console.log(`Yes I am taking the course: ${course.code} - ${course.name}`)
+        flagState = true;
+    }
+}
+
+if (flagState == false) {
+    courseList.push({ code: input, name: null })
+    console.log(`${input} has successfully been added.`)
+    console.log(courseList)
+}
